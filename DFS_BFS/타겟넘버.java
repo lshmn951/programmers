2dafd72c29f71a3036a37b5e696eadac3251b65c
lshmn951 <45991589+lshmn951@users.noreply.github.com>
2020-02-08 12:17:18 +0900

    타겟넘버

---
+++ DFS_BFS/타겟넘버.java
@@ -0,0 +1,32 @@
+package programmers;
+
+public class 타겟넘버 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	class Solution {
+		int cnt;
+	    public int solution(int[] numbers, int target) {
+	        int answer = 0;
+	        cnt =0;
+	        dfs(0,numbers,0,target);
+	        return cnt;
+	    }
+	    public void dfs(int k,int[] numbers,int temp,int target) {
+			if(k==numbers.length) {
+				if(temp==target) {
+					cnt++;
+				}
+				return;
+			}
+			else {
+				dfs(k+1,numbers,temp+numbers[k],target);
+				dfs(k+1,numbers,temp-numbers[k],target);
+				
+			}
+		}
+	}
+	
+}
